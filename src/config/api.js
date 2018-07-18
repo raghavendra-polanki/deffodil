b6b4302779c617daeb7ca4a5de373b9fa1f87253
raghavendra-polanki <raghavendra@HGB34ZJ.local>
2018-07-18 20:08:07 +0530

    added production variable in config

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,24 @@
-module.exports = {
+function config(){
+    
+    let apiUrl;
 
-    'adminApi' : {
-        'url'      : 'http://localhost:17883/api/',
+    if(process.env.REACT_APP_NODE_ENV === 'production'){
+        apiUrl = {
+
+            'adminApi' : {
+                'url'      : 'http://api.opino.me/api/'
+            }
+        }
+    }else{
+        apiUrl = {
+
+            'adminApi' : {
+                'url'      : 'http://localhost:17883/api/'
+            }
+        }
+
     }
+    return apiUrl
+}
 
-};
+export default config()
